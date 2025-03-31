9fd6b0ee313ec16a189c2769b008c674b14dfe21
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-31 15:27:57 -0500

    IMPORT scanner to use to capture user input,create scanner object for user input,creat string variable,ask user for input,save scanner object to string variable,use string variable to first replace all spaces with no spaces, then lower all characters to lowercase,create array variable that has the string variable as the array, then split the string into the array,use for loop to loop through array and check if all of the characters in the string are correctly split and displayed

--- App.java
+++ App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+// IMPORT scanner to use to capture user input
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // create scanner object for user input
+        Scanner sc = new Scanner(System.in);
+        // creat string variable
+        String word;
+        // ask user for input
+        System.out.println("Enter a input so we can count the the frequencies: ");
+        // save scanner object to string variable
+        word = sc.nextLine();
+        // use string variable to first replace all spaces with no spaces, then lower all characters to lowercase
+        word = word.replaceAll("\\s", "").toLowerCase();
+        // create array variable that has the string variable as the array, then split the string into the array
+        String[] wrdArray = word.split("");
+        // use for loop to loop through array and check if all of the characters in the string are correctly split and displayed
+        for (String string : wrdArray) {
+            System.out.println(string);
+        }
     }
+
 }
